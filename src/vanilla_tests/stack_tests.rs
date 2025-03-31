c47fb2c8b11ee643d9717e47e1298d5cc6f388ab
sjohn198 <sjohn198@calpoly.edu>
2025-03-31 09:42:34 -0700

    added many pop test

--- src/vanilla_tests/stack_tests.rs
+++ src/vanilla_tests/stack_tests.rs
@@ -32,5 +32,22 @@ fn pop_one_from_stack() {
     let mut stack: VectorStack<&str> = VectorStack::new();
     stack.push("Testing");
     assert_eq!(stack.pop(), Some("Testing"));
+    assert_eq!(stack.pop(), None);
+    assert_eq!(stack.peek(), None);
+}
 
+#[test]
+fn pop_many_from_stack() {
+    let mut stack: VectorStack<u16> = VectorStack::new();
+    stack.push(700);
+    stack.push(699);
+    stack.push(698);
+    stack.push(697);
+
+    assert_eq!(stack.pop(), Some(697));
+    assert_eq!(stack.pop(), Some(698));
+    assert_eq!(stack.pop(), Some(699));
+    assert_eq!(stack.pop(), Some(700));
+    assert_eq!(stack.pop(), None);
+    assert_eq!(stack.peek(), None);
 }
