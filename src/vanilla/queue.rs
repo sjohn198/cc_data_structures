adc83f6dd4a75db79643c7c8fbf093a2567d1163
sjohn198 <sjohn198@calpoly.edu>
2025-04-01 08:55:19 -0700

    queue implementation

---
+++ src/vanilla/queue.rs
@@ -0,0 +1,52 @@
+use std::collections::VecDeque;
+
+pub struct Queue<T> {
+    elements: VecDeque<T>,
+    max_size: Option<usize>
+}
+
+impl<T> Queue<T>{
+    /*initialize a new queue with optional max_size argument*/
+    pub fn new(max_size: Option<usize>) -> Self{
+        Queue {
+            elements: VecDeque::new(),
+            max_size
+        }
+    }
+
+    /*enqueue an item if possible*/
+    pub fn enqueue(&mut self, item: T) -> Result<(), &'static str> {
+        if let Some(limit) = self.max_size {
+            if self.elements.len() >= limit {
+                return Err("Queue overflow: max size reached");
+            }
+        }
+        self.elements.push_back(item);
+        Ok(())
+    }
+
+    /*remove an element from the head of the queue */
+    pub fn dequeue(&mut self) -> Option<T> {
+        self.elements.pop_front()
+    }
+
+    /*return a reference to the element at the head of the queue */
+    pub fn peek(&mut self) -> Option<&T> {
+        self.elements.front()
+    }
+
+    /*return a boolean indicating whether the queue is empty */
+    pub fn is_empty(&mut self) -> bool {
+        self.elements.is_empty()
+    }
+
+    /*return the number of elements in the queue */
+    pub fn size(&mut self) -> usize {
+        self.elements.len()
+    }
+
+    /*return the option representing the max_size if it was set */
+    pub fn get_max(&mut self) -> Option<usize> {
+        self.max_size
+    }
+}
