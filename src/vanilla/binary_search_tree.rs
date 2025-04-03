2743dbfa3e51e477a0138ec34c5fef178161b52a
sjohn198 <sjohn198@calpoly.edu>
2025-04-03 10:23:16 -0700

    search by key, find min/max, dfs and bfs traversals w/ tests for all

--- src/vanilla/binary_search_tree.rs
+++ src/vanilla/binary_search_tree.rs
@@ -1,3 +1,6 @@
+use std::cmp::max;
+use crate::vanilla::queue::Queue;
+
 pub struct Node<T> {
     key: i32,
     data: T,
@@ -17,13 +20,15 @@ impl<T> Node<T> {
 }
 
 pub struct BinarySearchTree<T> {
-    root: Option<Box<Node<T>>>
+    pub root: Option<Box<Node<T>>>,
+    pub size: usize
 }
 
 impl<T> BinarySearchTree<T> {
-    pub fn new(root: Option<Box<Node<T>>>) -> Self {
+    pub fn new() -> Self {
         BinarySearchTree {
-            root
+            root: None,
+            size: 0
         }
     }
 
@@ -31,24 +36,34 @@ impl<T> BinarySearchTree<T> {
         self.root.is_none()
     }
 
-    pub fn search(&mut self, key: i32) -> bool {
-        Self::search_helper(&self.root, key)
-    }
-
-    fn search_helper(node: &Option<Box<Node<T>>>, key: i32) -> bool {
+    fn search_helper(node: &Option<Box<Node<T>>>, key: i32) -> Option<&Box<Node<T>>> {
         match node {
             Some(box_node) => {
                 if box_node.key == key {
-                    true
-                } else if Self::search_helper(&box_node.left, key) {
-                    true
-                } else if Self::search_helper(&box_node.right, key) {
-                    true
+                    Some(box_node)
+                } else if box_node.key > key {
+                    Self::search_helper(&box_node.left, key)
                 } else {
-                    false
+                    Self::search_helper(&box_node.right, key)
                 }
             }
+            None => None
+        }
+    }
+
+    pub fn contains(&mut self, key: i32) -> bool {
+        match Self::search_helper(&self.root, key) {
+            Some(_) => true,
             None => false
+        }
+    }
+
+    pub fn find(&mut self, key: i32) -> Option<&T> {
+        match Self::search_helper(&self.root, key) {
+            Some(box_node) => {
+                Some(&box_node.data)
+            }
+            None => None
         }
     }
 
@@ -56,8 +71,10 @@ impl<T> BinarySearchTree<T> {
         if self.root.is_none() {
             let box_node: Box<Node<T>> = Box::new(Node::new(key, data, None, None));
             self.root = Some(box_node);
+            self.size += 1;
         } else{
-            Self::insert_helper(&mut self.root, key, data)
+            Self::insert_helper(&mut self.root, key, data); 
+            self.size += 1;
         }
     }
 
@@ -85,5 +102,143 @@ impl<T> BinarySearchTree<T> {
             }
             None => ()
         }
+    }
+
+    pub fn find_min(&mut self) -> Result<i32, &'static str> {
+        Self::find_min_helper(&self.root)
+    }
+
+    fn find_min_helper(node: &Option<Box<Node<T>>>) -> Result<i32, &'static str> {
+        match node {
+            Some(box_node) => {
+                if box_node.left.is_none() {
+                    Ok(box_node.key)
+                } else {
+                    Self::find_min_helper(&(box_node.left))
+                }
+            }
+            None => Err("Attempting to find min of empty tree")
+        }
+    }
+
+    pub fn find_max(&mut self) -> Result<i32, &'static str> {
+        Self::find_max_helper(&self.root)
+    }
+
+    fn find_max_helper(node: &Option<Box<Node<T>>>) -> Result<i32, &'static str> {
+        match node {
+            Some(box_node) => {
+                if box_node.right.is_none() {
+                    Ok(box_node.key)
+                } else {
+                    Self::find_max_helper(&(box_node.right))
+                }
+            }
+            None => Err("Attempting to find max of empty tree")
+        }
+    }
+
+    pub fn tree_height(&mut self) -> i32 {
+        Self::tree_height_helper(&self.root)
+    }
+
+    fn tree_height_helper(node: &Option<Box<Node<T>>>) -> i32 {
+        match node {
+            Some(box_node) => {
+                if box_node.left.is_none() && box_node.right.is_none() {
+                    0
+                } else {
+                    1 + max(Self::tree_height_helper(&box_node.left), Self::tree_height_helper(&box_node.right))
+                }
+            }
+            None => 0
+        }
+    }
+
+    pub fn inorder_traversal(&mut self) -> Vec<i32> {
+        let mut vec: Vec<i32> = Vec::with_capacity(self.size);
+        Self::inorder_helper(&self.root, &mut vec);
+        vec
+    }
+
+    fn inorder_helper(node: &Option<Box<Node<T>>>, vec: &mut Vec<i32>) {
+        match node {
+            Some(box_node) => {
+                Self::inorder_helper(&box_node.left, vec);
+                vec.push(box_node.key);
+                Self::inorder_helper(&box_node.right, vec);
+            }
+            None => ()
+        }
+    }
+
+    pub fn preorder_traversal(&mut self) -> Vec<i32> {
+        let mut vec: Vec<i32> = Vec::with_capacity(self.size);
+        Self::preorder_helper(&self.root, &mut vec);
+        vec
+    }
+
+    fn preorder_helper(node: &Option<Box<Node<T>>>, vec: &mut Vec<i32>) {
+        match node {
+            Some(box_node) => {
+                vec.push(box_node.key);
+                Self::preorder_helper(&box_node.left, vec);
+                Self::preorder_helper(&box_node.right, vec);
+            }
+            None => ()
+        }
+    }
+
+    pub fn postorder_traversal(&mut self) -> Vec<i32> {
+        let mut vec: Vec<i32> = Vec::with_capacity(self.size);
+        Self::postorder_helper(&self.root, &mut vec);
+        vec
+    }
+
+    fn postorder_helper(node: &Option<Box<Node<T>>>, vec: &mut Vec<i32>) {
+        match node {
+            Some(box_node) => {
+                Self::postorder_helper(&box_node.left, vec);
+                Self::postorder_helper(&box_node.right, vec);
+                vec.push(box_node.key);
+            }
+            None => ()
+        }
+    }
+
+    pub fn bfs_traversal(&mut self) -> Vec<i32> {
+        let mut q: Queue<&Option<Box<Node<T>>>> = Queue::new(None); 
+        let mut trav: Vec<i32> = Vec::with_capacity(self.size);
+
+        match q.enqueue(&self.root) {
+            Ok(_) => {},
+            Err(_) => return Vec::new()
+        };
+
+        while !q.is_empty() {
+            let temp = q.dequeue();
+            match temp {
+                Some(option_node) => {
+                    match option_node {
+                        Some(box_node) => {
+                            trav.push(box_node.key);
+                            
+                            match q.enqueue(&box_node.left) {
+                                Ok(_) => {},
+                                Err(_) => return Vec::new()
+                            };
+
+                            match q.enqueue(&box_node.right) {
+                                Ok(_) => {},
+                                Err(_) => return Vec::new()
+                            };
+                        }
+                        None => {}
+                    }
+                }
+                None => {}
+            }
+        }
+        trav
     }
 }
--- src/vanilla_tests/bst_tests.rs
+++ src/vanilla_tests/bst_tests.rs
@@ -2,31 +2,197 @@ use crate::vanilla::binary_search_tree::BinarySearchTree;
 
 #[test]
 fn new_bst() {
-    let mut bst: BinarySearchTree<i32> = BinarySearchTree::new(None);
+    let mut bst: BinarySearchTree<i32> = BinarySearchTree::new();
     assert!(bst.is_empty());
 }
 
 #[test]
 fn insert_one_node() {
-    let mut bst: BinarySearchTree<String> = BinarySearchTree::new(None);
-    bst.insert(12, "Test".to_string());
-    assert!(bst.search(12));
+    let mut bst: BinarySearchTree<&str> = BinarySearchTree::new();
+    bst.insert(12, "Test");
+    assert!(bst.contains(12));
+    assert_eq!(bst.find(12), Some(&"Test"));
+    assert_eq!(bst.tree_height(), 0);
+    assert_eq!(bst.size, 1);
 }
 
 #[test]
 fn insert_many_node() {
-    let mut bst: BinarySearchTree<char> = BinarySearchTree::new(None);
+    let mut bst: BinarySearchTree<char> = BinarySearchTree::new();
     bst.insert(12, 'a');
-    assert!(bst.search(12));
+    assert!(bst.contains(12));
+    assert_eq!(bst.find(12), Some(&'a'));
+    assert_eq!(bst.tree_height(), 0);
 
     bst.insert(10, 'b');
-    assert!(bst.search(10));
+    assert!(bst.contains(10));
+    assert_eq!(bst.find(10), Some(&'b'));
+    assert_eq!(bst.tree_height(), 1);
 
     bst.insert(13, 'c');
-    assert!(bst.search(13));
+    assert!(bst.contains(13));
+    assert_eq!(bst.find(13), Some(&'c'));
+    assert_eq!(bst.tree_height(), 1);
 
     bst.insert(11, 'd');
-    assert!(bst.search(11));
+    assert!(bst.contains(11));
+    assert_eq!(bst.find(11), Some(&'d'));
+    assert_eq!(bst.tree_height(), 2);
 
-    assert!(!bst.search(14));
+    assert!(!bst.contains(14));
+    assert_eq!(bst.find(14), None);
+    assert_eq!(bst.size, 4);
+}
+
+#[test]
+fn find_min_simple() {
+    let mut bst: BinarySearchTree<f64> = BinarySearchTree::new();
+    bst.insert(12, 11.12);
+    bst.insert(11, 100000.0);
+    assert_eq!(bst.find_min(), Ok(11));
+    assert_eq!(bst.size, 2);
+}
+
+#[test]
+fn find_min_more_complex() {
+    let mut bst: BinarySearchTree<f64> = BinarySearchTree::new();
+    bst.insert(11, 11.11);
+    bst.insert(12, 12.12);
+    bst.insert(9, 9.99);
+    bst.insert(10, 10.0110);
+    bst.insert(8, 8.88);
+    assert_eq!(bst.find_min(), Ok(8));
+    assert_eq!(bst.size, 5);
+}
+
+#[test]
+fn find_max_simple() {
+    let mut bst: BinarySearchTree<f64> = BinarySearchTree::new();
+    bst.insert(12, 11.12);
+    bst.insert(13, 100000.0);
+    assert_eq!(bst.find_max(), Ok(13));
+    assert_eq!(bst.size, 2);
+}
+
+#[test]
+fn find_max_more_complex() {
+    let mut bst: BinarySearchTree<f64> = BinarySearchTree::new();
+    bst.insert(11, 11.11);
+    bst.insert(12, 12.12);
+    bst.insert(10, 10.0110);
+    bst.insert(16, 16.16);
+    bst.insert(15, 15.15);
+    bst.insert(17, 17.17);
+    bst.insert(14, 14.14);
+    assert_eq!(bst.find_max(), Ok(17));
+    assert_eq!(bst.size, 7);
+}
+
+#[test]
+fn find_min_and_max() {
+    let mut bst: BinarySearchTree<f64> = BinarySearchTree::new();
+    bst.insert(11, 11.11);
+    assert_eq!(bst.tree_height(), 0);
+
+    bst.insert(12, 12.12);
+    assert_eq!(bst.tree_height(), 1);
+
+    bst.insert(9, 9.99);
+    assert_eq!(bst.tree_height(), 1);
+
+    bst.insert(10, 10.0110);
+    assert_eq!(bst.tree_height(), 2);
+
+    bst.insert(8, 8.88);
+    assert_eq!(bst.tree_height(), 2);
+
+    bst.insert(16, 16.16);
+    assert_eq!(bst.tree_height(), 2);
+
+    bst.insert(15, 15.15);
+    assert_eq!(bst.tree_height(), 3);
+
+    bst.insert(14, 14.14);
+    assert_eq!(bst.tree_height(), 4);
+
+    bst.insert(17, 17.17);
+    assert_eq!(bst.tree_height(), 4);
+
+    assert_eq!(bst.find_min(), Ok(8));
+    assert_eq!(bst.find_max(), Ok(17));
+    assert!(bst.contains(8));
+    assert_eq!(bst.size, 9);
+}
+
+#[test]
+fn find_w_change() {
+    let mut bst: BinarySearchTree<u16> = BinarySearchTree::new();
+    bst.insert(11, 63);
+    assert_eq!(bst.find(11), Some(&63));
+    bst.insert(11, 56);
+    assert_eq!(bst.find(11), Some(&56));
+}
+
+#[test]
+fn preorder_test() {
+    let mut bst: BinarySearchTree<f32> = BinarySearchTree::new();
+    bst.insert(11, 11.11);
+    bst.insert(12, 12.12);
+    bst.insert(9, 9.99);
+    bst.insert(10, 10.0110);
+    bst.insert(8, 8.88);
+    bst.insert(16, 16.16);
+    bst.insert(15, 15.15);
+    bst.insert(14, 14.14);
+    bst.insert(17, 17.17);
+
+    assert_eq!(bst.preorder_traversal(), vec![11, 9, 8, 10, 12, 16, 15, 14, 17]);
+}
+
+#[test]
+fn inorder_test() {
+    let mut bst: BinarySearchTree<f32> = BinarySearchTree::new();
+    bst.insert(11, 11.11);
+    bst.insert(12, 12.12);
+    bst.insert(9, 9.99);
+    bst.insert(10, 10.0110);
+    bst.insert(8, 8.88);
+    bst.insert(16, 16.16);
+    bst.insert(15, 15.15);
+    bst.insert(14, 14.14);
+    bst.insert(17, 17.17);
+
+    assert_eq!(bst.inorder_traversal(), vec![8, 9, 10, 11, 12, 14, 15, 16, 17]);
+}
+
+#[test]
+fn postorder_test() {
+    let mut bst: BinarySearchTree<f32> = BinarySearchTree::new();
+    bst.insert(11, 11.11);
+    bst.insert(12, 12.12);
+    bst.insert(9, 9.99);
+    bst.insert(10, 10.0110);
+    bst.insert(8, 8.88);
+    bst.insert(16, 16.16);
+    bst.insert(15, 15.15);
+    bst.insert(14, 14.14);
+    bst.insert(17, 17.17);
+
+    assert_eq!(bst.postorder_traversal(), vec![8, 10, 9, 14, 15, 17, 16, 12, 11]);
+}
+
+#[test]
+fn bfs_test() {
+    let mut bst: BinarySearchTree<f32> = BinarySearchTree::new();
+    bst.insert(11, 11.11);
+    bst.insert(12, 12.12);
+    bst.insert(9, 9.99);
+    bst.insert(10, 10.0110);
+    bst.insert(8, 8.88);
+    bst.insert(16, 16.16);
+    bst.insert(15, 15.15);
+    bst.insert(14, 14.14);
+    bst.insert(17, 17.17);
+
+    assert_eq!(bst.bfs_traversal(), vec![11, 9, 12, 8, 10, 16, 15, 17, 14]);
 }
